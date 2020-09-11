f23760a1e126e961450c1eeb2c26696ec8fd565a
unknown <james@lightestnight.com>
2020-09-11 17:44:16 +0100

    Delegate DI to SqlStreamStore.MySql

--- MySql/ExtendsServiceCollection.cs
+++ MySql/ExtendsServiceCollection.cs
@@ -1,10 +1,7 @@
 ﻿using System;
 using LightestNight.System.Data.MySql;
-using LightestNight.System.EventSourcing.Checkpoints;
-using LightestNight.System.EventSourcing.SqlStreamStore.MySql.Checkpoints;
+using LightestNight.System.EventSourcing.SqlStreamStore.MySql;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.DependencyInjection.Extensions;
-using SqlStreamStore;
 
 namespace LightestNight.System.EventSourcing.SqlStreamStore.Subscriptions.MySql
 {
@@ -12,29 +9,7 @@ namespace LightestNight.System.EventSourcing.SqlStreamStore.Subscriptions.MySql
     {
         public static IServiceCollection AddEventStoreSubscriptions(this IServiceCollection services,
             Action<MySqlOptions> mysqlOptions, Action<EventSourcingOptions>? eventSourcingOptions = null)
-        {
-            var options = new EventSourcingOptions();
-            eventSourcingOptions?.Invoke(options);
-
-            services.AddMySqlData(mysqlOptions);
-
-            var serviceProvider = services.BuildServiceProvider();
-            if (!(serviceProvider.GetService<IStreamStore>() is MySqlStreamStore))
-            {
-                services.AddSingleton<IStreamStore>(sp =>
-                {
-                    var connectionString = sp.GetRequiredService<IMySqlConnection>().Build().ConnectionString;
-                    return new MySqlStreamStore(new MySqlStreamStoreSettings(connectionString));
-                });
-            }
-            
-            services.TryAddSingleton<GetGlobalCheckpoint>(sp =>
-                sp.GetRequiredService<MySqlCheckpointManager>().GetGlobalCheckpoint);
-
-            services.TryAddSingleton<SetGlobalCheckpoint>(sp =>
-                sp.GetRequiredService<MySqlCheckpointManager>().SetGlobalCheckpoint);
-
-            return services.AddHostedService<EventSubscription>();
-        } 
+            => services.AddMySqlEventStore(mysqlOptions, eventSourcingOptions)
+                .AddHostedService<EventSubscription>();
     }
 }

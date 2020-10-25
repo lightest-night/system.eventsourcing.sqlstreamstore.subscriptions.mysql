b4b03131b76d02aa8ce0d08ded6536c857e8a47c
James Woodley <james@lightestnight.com>
2020-10-25 23:02:40 +0000

    Use the new MySqlOptionsFactory

--- MySql/ExtendsServiceCollection.cs
+++ MySql/ExtendsServiceCollection.cs
@@ -8,8 +8,8 @@ namespace LightestNight.System.EventSourcing.SqlStreamStore.Subscriptions.MySql
     public static class ExtendsServiceCollection
     {
         public static IServiceCollection AddEventStoreSubscriptions(this IServiceCollection services,
-            Action<MySqlOptions> mysqlOptions, Action<EventSourcingOptions>? eventSourcingOptions = null)
-            => services.AddMySqlEventStore(mysqlOptions, eventSourcingOptions)
+            MySqlOptionsFactory mySqlOptionsFactory, Action<EventSourcingOptions>? eventSourcingOptions = null)
+            => services.AddMySqlEventStore(mySqlOptionsFactory, eventSourcingOptions)
                 .AddHostedService<EventSubscription>();
     }
 }
